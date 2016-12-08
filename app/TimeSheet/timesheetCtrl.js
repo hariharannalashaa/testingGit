66a66e271f1e5adac69fbd1697221e6e1dc57d3f
HariHaran <hariharan@nalashaa.com>
2016-12-08 09:16:23 +0530

    Rendering of timesheet

--- app/TimeSheet/timesheetCtrl.js
+++ app/TimeSheet/timesheetCtrl.js
@@ -4,10 +4,23 @@ var timesheetCtrl = function(TimeSheetFactory,timeSheetData,projects,tasks){
     var currentDate = new Date();
     that.displayMonth = currentDate.getMonthName();
     that.timeSheet = {};
+    that.projects = angular.copy(timeSheetData.distinctProjects);
+    that.projectlist = angular.copy(projects);
+    that.taskList = angular.copy(tasks);
+    that.totalHours = {};
+    that.approveMatrix = {};
+    that.submittedMatrix = {};
+    
     generateDateForMonth();
-    that.projects = timeSheetData.distinctProjects;
-    that.projectlist = projects;
-    that.taskList = tasks;
+    processTimeSheetData();
+    
+    function processTimeSheetData(){
+        for(var key in timeSheetData.work){
+            for(var i = 0 ; i < timeSheetData.work[key].length; i++){
+                that.projects[timeSheetData.work[key][i].index-1].times[key].hoursSpent = timeSheetData.work[key][i].hoursSpent;
+            }
+        }
+    }
     
     function changeMonth(forward){
         if(forward){
@@ -29,13 +42,18 @@ var timesheetCtrl = function(TimeSheetFactory,timeSheetData,projects,tasks){
         lastDay.setDate(0);
         for(var i = firstDay ; i <= lastDay ; i.setDate(i.getDate()+1)){
             var date = moment(i);
-            var key = date.format("DD-MM-YYYY");
-            var column = {
+            var key = date.format("YYYY-MM-DD");
+            var columns = {
                 displayDate : date.format("DD"),
-                times : [1,2,3]
             }
-            that.timeSheet[key] = column;
+            that.timeSheet[key] = columns;
         }
+        for(var i = 0 ; i < that.projects.length; i++){
+            that.projects[i].times = angular.copy(that.timeSheet);
+        }
+        that.totalHours = angular.copy(that.timeSheet);
+        that.approveMatrix = angular.copy(that.timeSheet);
+        that.submittedMatrix = angular.copy(that.timeSheet);
     }
     
     
